48df2d828da9f08915163fa04cf86f3aeef07478
Károly Török <torok.karoly.krisztian@gmail.com>
2025-05-23 14:25:14 +0200

    React support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    port: process.env.PORT ? Number(process.env.PORT) : 60_001,
+  },
+
+  vite: {
+    worker: { format: "es" },
+  },
+
+  integrations: [react()],
+});
